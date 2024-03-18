ac16c1580c518f831afb736a64ee3c17c11e13c8
Chris Swanda <chris@PorkRibs.local>
2024-03-18 11:52:38 -0500

    halving days counter

--- cbbi.py
+++ cbbi.py
@@ -1,4 +1,14 @@
 import requests
+from datetime import datetime
+
+# Current date
+current_date = datetime.now()
+
+# Target date
+target_date = datetime(2024, 4, 18)
+
+# Calculate the difference in days
+days_until_target = (target_date - current_date).days
 
 # URL of the API
 url = "https://colintalkscrypto.com/cbbi/data/latest.json"
@@ -30,6 +40,7 @@ if response.status_code == 200:
     # Convert values to percentages in whole numbers with percent sign
     confidence_values_percentage = [f"{round(value * 100)}%" for value in confidence_values]
 
+    print("Estimated days until the Halving:", days_until_target)
     # Print the trend and the last 7 CBBI values as percentages with percent sign
     print("Trend:", trend)
     print("Last 7 CBBI values:", ", ".join(confidence_values_percentage))
